20857c77d5e41e225e2af3fd6d39359db38ee08f
Alejandro GX <alex@flyingpigs.es>
2018-07-06 11:57:56 +0200

    skin update

--- tecina-api/resources/views/admin/dish.blade.php
+++ tecina-api/resources/views/admin/dish.blade.php
@@ -34,7 +34,12 @@
                     <tr>
                       <td>{{$dish->id}}</td>
                       <td>{{$dish->name}}</td>
-                      <td><a href="/api/dishes/{{$dish->id}}/edit" class="btn btn-primary a-btn-slide-text"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span><span><strong>Editar</strong></span></a></td>
+                      <td>
+                        <a href="/api/dishes/{{$dish->id}}/edit" class="btn btn-primary">
+                          <i class="material-icons">edit</i>
+                          <span>Editar</span>
+                        </a>
+                      </td>
                     </tr>
                   @endforeach
                 </tbody>
--- tecina-api/resources/views/admin/dish_edit.blade.php
+++ tecina-api/resources/views/admin/dish_edit.blade.php
@@ -1,123 +1,150 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="row justify-content-center">
+<div class="container">
+  <div class="row justify-content-center">
 
-  <div class="col-md-8">
+    <div class="col-md-8">
 
-  <div class="card">
-    <div class="card-header">
-      <strong>Edición de Plato</strong>
-    </div>
-    <div class="card-body">
-      <form action="/api/dishes/{{$dish->id}}" method="POST">
-        <input name="_method" type="hidden" value="PUT">
-        <ul class="nav nav-tabs">
-          @foreach($langs =  DB::table('languages')->get() as $lang)
-            <li @if ($loop->first) class="active show" @endif><a data-toggle="tab" href="#{{$lang->code}}">{{$lang->code}}</a></li>
-          @endforeach
-        </ul>
-        <div class="tab-content">
-          @foreach($langs =  DB::table('languages')->get() as $lang)
-          <div id="{{$lang->code}}" class="tab-pane fade in @if ($loop->first) active show @endif">
-            <label for="name_{{$lang->code}}">
-              <span>Nombre</span>
-              <input type="text" id="name_{{$lang->code}}" name="name_{{$lang->code}}" value="{{ @$translations[$lang->code]['name'] }}"/>
-            </label>
-            <label for="description_{{$lang->code}}">
-              <span>Descripción</span>
-              <input type="text" id="description_{{$lang->code}}" name="description_{{$lang->code}}" value="{{ @$translations[$lang->code]['description'] }}"/>
-            </label>
-          </div>
-          @endforeach
+      <div class="card">
+        <div class="card-header">
+          <strong>Edición de Plato</strong>
         </div>
-        <div>
-          <label for="ingredients">
-            <span>Ingredientes</span>
-          <input type="text" name="ingredients" value="{{($dish->ingredients)?$dish->ingredients:''}}" />
-        </label>
-        </div>
-        <div>
-          <label for="active">
-            <span>Activo</span>
-          <input type="checkbox" name="active"{{($dish->active)?' checked':''}}  />
-        </label>
-        </div>
-        <input type="submit" value="Guardar cambios" />
-      </form>
-          <div>
-            <span>Alérgenos</span>
-            <ul id="dish_allergens">
-              @foreach($allergens as $allergen)
-                <li id="allergen_{{$allergen->id}}"><span class="allergenName">{{@$allergenTranslations[$allergen->id]['es']['name']}}</span> <span class="glyphicon glyphicon-remove-circle"><a href="#" onclick="deleteDishAllergen({{$dish->id}},{{$allergen->id}});">Eliminar</a></span></li>
-              @endforeach
-            </ul>
-          <label for="add_dish_allergen">
-            <span>Selecciona un alérgeno</span>
-            <select id="add_dish_allergen">
-              @foreach(db::table('allergens_translations')->whereNotIn('id_allergen',$allergenIds)->where('id_language',1)->get() as $allergen)
-                <option value="{{$allergen->id_allergen}}">
-                  {{$allergenTranslations[$allergen->id_allergen][$lang->code]['name']}}
-                </option>
-              @endforeach
-            </select>
-          </label>
-          <button type="button" name="button" id="add_dish_allergen_button" onclick="addDishAllergen({{$dish->id}})">Añadir alérgeno</button>
-          </div>
+        <div class="card-body">
 
-          <div>
-            <span>Tipo de plato</span>
-            <ul id="dish_food_type">
-              @foreach($foodTypes as $food_type)
-                <li id="food_type_{{$food_type->id}}"><span class="foodTypeName">{{@$foodTypeTranslations[$food_type->id]['es']}}</span> <span class="glyphicon glyphicon-remove-circle"><a href="#" onclick="deleteDishFoodType({{$dish->id}},{{$food_type->id}});">Eliminar</a></span></li>
-              @endforeach
-            </ul>
-          <label for="add_dish_food_type">
-            <span>Selecciona un tipo de plato</span>
-            <select id="add_dish_food_type">
-              @foreach(db::table('food_types_translations')->whereNotIn('id_food_type',$foodTypeIds)->where('id_language',1)->get() as $foodType)
-                <option value="{{$foodType->id_food_type}}">
-                  {{$foodTypeTranslations[$foodType->id_food_type]['es']}}
-                </option>
-              @endforeach
-            </select>
-          </label>
-          <button type="button" name="button" id="add_dish_foodType_button" onclick="addDishFoodType({{$dish->id}})">Añadir tipo de plato</button>
-          </div>
+          <section>
+            <p>Información general</p>
+            <form action="/api/dishes/{{$dish->id}}" method="POST">
+              <input name="_method" type="hidden" value="PUT">
+              <ul class="nav nav-tabs">
+                @foreach($langs =  DB::table('languages')->get() as $lang)
+                  <li><a data-toggle="tab" href="#{{$lang->code}}" @if($loop->first) class="active show" @endif>{{$lang->code}}</a></li>
+                @endforeach
+              </ul>
+              <div class="tab-content">
+                @foreach($langs =  DB::table('languages')->get() as $lang)
+                <div id="{{$lang->code}}" class="tab-pane fade in @if ($loop->first) active show @endif">
+                  <label for="name_{{$lang->code}}">
+                    <span>Nombre:</span>
+                    <input type="text" id="name_{{$lang->code}}" name="name_{{$lang->code}}" value="{{ @$translations[$lang->code]['name'] }}"/>
+                  </label>
+                  <label for="description_{{$lang->code}}">
+                    <span>Descripción:</span>
+                    <input type="text" id="description_{{$lang->code}}" name="description_{{$lang->code}}" value="{{ @$translations[$lang->code]['description'] }}"/>
+                  </label>
+                </div>
+                @endforeach
+              </div>
+              <label for="ingredients">
+                <span>Ingredientes:</span>
+                <input type="text" name="ingredients" value="{{($dish->ingredients)?$dish->ingredients:''}}" />
+              </label>
+              <label for="active">
+                <span>Activo:</span>
+                <input type="checkbox" name="active"{{($dish->active)?' checked':''}}  />
+              </label>
+              <button type="submit" class="btn btn-primary">
+                <i class="material-icons">save</i>
+                <span>Guardar cambios</span>
+              </button>
+            </form>
+          </section>
 
-          <div>
-            <span>Categoría de plato</span>
+          <section>
+            <p>Categoría de plato</p>
             <ul id="dish_category">
               @foreach($categories as $category)
                 <li id="category_{{$category->id}}"><span class="categoryName">{{@$categoryTranslations[$category->id]['es']['name']}}</span> <span class="glyphicon glyphicon-remove-circle"><a href="#" onclick="deleteDishCategory({{$dish->id}},{{$category->id}});">Eliminar</a></span></li>
               @endforeach
             </ul>
+            <label for="add_dish_category">
+              <span>Selecciona una categoría:</span>
+              <select id="add_dish_category">
+                @foreach(db::table('categories_translations')->whereNotIn('id_category',$categoryIds)->where('id_language',1)->get() as $category)
+                  <option value="{{$category->id_category}}">
+                    {{$category->name}}
+                  </option>
+                @endforeach
+              </select>
+            </label>
+            <button type="button" class="btn btn-primary" id="add_dish_category_button" onclick="addDishCategory({{$dish->id}})">
+              <i class="material-icons">add_circle</i>
+              <span>Añadir categoría de plato</span>
+            </button>
+          </section>
 
-          <label for="add_dish_category">
-            <span>Selecciona una categoría</span>
-            <select id="add_dish_category">
-              @foreach(db::table('categories_translations')->whereNotIn('id_category',$categoryIds)->where('id_language',1)->get() as $category)
-                <option value="{{$category->id_category}}">
-                  {{$category->name}}
-                </option>
+          <section>
+            <p>Tipo de plato</p>
+            <ul id="dish_food_type">
+              @foreach($foodTypes as $food_type)
+                <li id="food_type_{{$food_type->id}}"><span class="foodTypeName">{{@$foodTypeTranslations[$food_type->id]['es']}}</span> <span class="glyphicon glyphicon-remove-circle"><a href="#" onclick="deleteDishFoodType({{$dish->id}},{{$food_type->id}});">Eliminar</a></span></li>
               @endforeach
-            </select>
-          </label>
-          <button type="button" name="button" id="add_dish_category_button" onclick="addDishCategory({{$dish->id}})">Añadir categoría</button>
-          </div>
+            </ul>
+            <label for="add_dish_food_type">
+              <span>Selecciona un tipo de plato:</span>
+              <select id="add_dish_food_type">
+                @foreach(db::table('food_types_translations')->whereNotIn('id_food_type',$foodTypeIds)->where('id_language',1)->get() as $foodType)
+                  <option value="{{$foodType->id_food_type}}">
+                    {{$foodTypeTranslations[$foodType->id_food_type]['es']}}
+                  </option>
+                @endforeach
+              </select>
+            </label>
+            <button type="button" class="btn btn-primary" id="add_dish_foodType_button" onclick="addDishFoodType({{$dish->id}})">
+              <i class="material-icons">add_circle</i>
+              <span>Añadir tipo de plato</span>
+            </button>
+          </section>
 
-          <img style="max-width:300px;max-height:300px;" id="dish_image" src="{{$dish->image}}" class="dish main admin" onclick="jQuery('#uploadDishImage').toggle();" />
-          <div id="uploadDishImage" style="display:none">
-            <input type="file" name="dishImage" id="dishImage" accept="image/x-png" placeholder="Imagen nueva" />
-              <button type="button" name="dishImage" id="upload_dish_image_button" onclick="uploadDishImage({{$dish->id}})">Cambiar imagen</button>
-          </div>
+          <section>
+            <p>Alérgenos</p>
+            <ul id="dish_allergens">
+              @foreach($allergens as $allergen)
+                <li id="allergen_{{$allergen->id}}">
+                  <span class="allergenName">{{@$allergenTranslations[$allergen->id]['es']['name']}}</span>
+                  <a href="#" onclick="deleteDishAllergen({{$dish->id}},{{$allergen->id}});" class="link">
+                    <i class="material-icons">delete</i>
+                    <span>Eliminar</span>
+                  </a>
+                </li>
+              @endforeach
+            </ul>
+            <label for="add_dish_allergen">
+              <span>Selecciona un alérgeno:</span>
+              <select id="add_dish_allergen">
+                @foreach(db::table('allergens_translations')->whereNotIn('id_allergen',$allergenIds)->where('id_language',1)->get() as $allergen)
+                  <option value="{{$allergen->id_allergen}}">
+                    {{$allergenTranslations[$allergen->id_allergen][$lang->code]['name']}}
+                  </option>
+                @endforeach
+              </select>
+            </label>
+            <button type="button" class="btn btn-primary" id="add_dish_allergen_button" onclick="addDishAllergen({{$dish->id}})">
+              <i class="material-icons">add_circle</i>
+              <span>Añadir alérgeno</span>
+            </button>
+          </section>
+
+          <section>
+            <p>Imagen del plato</p>
+            <img style="max-width:300px;max-height:300px;" id="dish_image" src="{{$dish->image}}" class="dish main admin" onclick="jQuery('#uploadDishImage').toggle();" />
+            <div id="uploadDishImage" style="display:none">
+              <label for="dishImage">
+                <span>Selecciona una imagen:</span>
+                <input type="file" name="dishImage" id="dishImage" accept="image/x-png" placeholder="Imagen nueva" />
+              </label>
+              <button type="button" class="btn btn-primary" id="upload_dish_image_button" onclick="uploadDishImage({{$dish->id}})">
+                <i class="material-icons">photo_camera</i>
+                <span>Cambiar imagen</span>
+              </button>
+            </div>
+          </section>
+
+        </div>
+      </div>
+
     </div>
-
-
-</div>
-</div>
-</div>
+  </div>
 </div>
 <script type="text/javascript">
   function addDishAllergen(dishId){
