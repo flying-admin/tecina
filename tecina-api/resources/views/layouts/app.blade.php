92d27de728633935f820f03d43d3c744691d8c1e
Alejandro GX <alex@flyingpigs.es>
2018-07-09 17:16:17 +0200

    api menu fix

--- tecina-api/resources/views/layouts/app.blade.php
+++ tecina-api/resources/views/layouts/app.blade.php
@@ -45,11 +45,30 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            <li class="nav-item">
+                            {{-- <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
+                            </li> --}}
                         @else
-                          <li class="nav-item dropdown">
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('dish') }}">Platos</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('menu') }}">Menús</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('wine') }}">Vinos</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('highlight') }}">Destacados</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#!">&nbsp;|&nbsp;</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{ route('logout') }}">{{ __('Logout') }}</a>
+                          </li>
+
+                          {{-- <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                               editar <span class="caret"></span>
                             </a>
@@ -68,24 +87,24 @@
                                 Listado de destacados
                               </a>
                             </div>
-                          </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                          </li> --}}
+                          {{-- <li class="nav-item dropdown">
+                              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                  {{ Auth::user()->name }} <span class="caret"></span>
+                              </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                  <a class="dropdown-item" href="{{ route('logout') }}"
+                                     onclick="event.preventDefault();
+                                                   document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                                  </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                      @csrf
+                                  </form>
+                              </div>
+                          </li> --}}
 
                         @endguest
                     </ul>
