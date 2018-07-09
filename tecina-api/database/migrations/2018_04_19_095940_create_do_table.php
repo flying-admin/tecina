110234572643d6a11bcb8f57ec323003e8460e8f
fp-ultano <ultano@flyingpigs.es>
2018-07-09 11:01:58 +0200

    added boolean canarias to denomination of origin model

--- tecina-api/database/migrations/2018_04_19_095940_create_do_table.php
+++ tecina-api/database/migrations/2018_04_19_095940_create_do_table.php
@@ -17,6 +17,7 @@ class CreateDoTable extends Migration
             $table->tinyIncrements('id');
             $table->string('name');
             $table->string('region')->nullable();
+            $table->boolean('canarias')->default(FALSE);
         });
     }
 
